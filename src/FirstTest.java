b2eeaa12cc55d4877e7b0f5b470a126089dc1d85
VladBrukh <vladimirbrukh@gmail.com>
2019-04-07 02:10:39 +0300

    lesson4_exercise1

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,16 +1,19 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class FirstTest {
 
@@ -39,23 +42,163 @@ public class FirstTest {
     }
 
     @Test
-    public void swipeArticle()
+    public void saveArticles()
     {
 
         waitForElementAndClick(
                 By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
                 "Cannot find 'Search Wikipedia' input",
-                5
+                25
         );
 
         waitForElementAndSendKeys(
                 By.id("org.wikipedia:id/search_src_text"),
-                "Java",
+                "Dracula",
                 "Cannot find search input",
-                15
+                25
         );
 
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/search_results_container']//*[contains(@text, '1897 Gothic horror novel by Irish author Bram Stoker')]"),
+                "Cannot find article with '1897 Gothic horror novel by Irish author Bram Stoker' text searching by 'Dracula'",
+                25
+        );
 
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageView[@content-desc='More options']"),
+                "Cannot find a button to open article options",
+                25
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.LinearLayout//*[@text='Add to reading list']"),
+                "Cannot find an option to add the article to reading list",
+                25
+        );
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/onboarding_button"),
+                "Cannot find 'Got it' tip overlay",
+                25
+        );
+
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/text_input"),
+                "Cannot find input to sen the article name",
+                25
+        );
+
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/text_input"),
+                "Dracula folder",
+                "Cannot input text into article folder input",
+                25
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text='OK']"),
+                "Cannot press OK button",
+                25
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Cannot close the article, cannot find X link",
+                25
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia' input",
+                25
+        );
+
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Dracula",
+                "Cannot find search input",
+                25
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/search_results_container']//*[contains(@text, '2014 American dark fantasy action horror film directed by Gary Shore')]"),
+                "Cannot find article with '2014 American dark fantasy action horror film directed by Gary Shore' text searching by 'Dracula'",
+                25
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageView[@content-desc='More options']"),
+                "Cannot find a button to open article options",
+                25
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.LinearLayout//*[@text='Add to reading list']"),
+                "Cannot find an option to add the article to reading list",
+                25
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text='Dracula folder']"),
+                "Cannot find created folder",
+                25
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Cannot close the article, cannot find X link",
+                25
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.FrameLayout[@content-desc='My lists']"),
+                "Cannot find navigation button to my lists",
+                25
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text='Dracula folder']"),
+                "Cannot find created folder",
+                25
+        );
+
+        swipeElementToLeft(
+                By.xpath("//*[@text='1897 Gothic horror novel by Irish author Bram Stoker']"),
+                "Cannot find saved article"
+        );
+
+        waitForElementNotPresent(
+                By.xpath("//*[@text='1897 Gothic horror novel by Irish author Bram Stoker']"),
+                "Cannot deleted saved article",
+                25
+        );
+
+        WebElement not_deleted_article = waitForElementPresent(
+                By.xpath("//*[@text='2014 American dark fantasy action horror film directed by Gary Shore']"),
+                "Cannot find article with '2014 American dark fantasy action horror film directed by Gary Shore' text searching by 'Dracula'",
+                25
+        );
+
+        String title_in_lists = not_deleted_article.getAttribute("text");
+
+        waitForElementAndClick(
+                By.xpath("//*[@text='2014 American dark fantasy action horror film directed by Gary Shore']"),
+                "Cannot find article with '2014 American dark fantasy action horror film directed by Gary Shore' text searching by 'Dracula'",
+                25
+        );
+
+        WebElement opened_article = waitForElementPresent(
+                By.xpath("//*[@text='2014 American dark fantasy action horror film directed by Gary Shore']"),
+                "Cannot find article with '2014 American dark fantasy action horror film directed by Gary Shore' text searching by 'Dracula'",
+                25
+        );
+
+        String title_opened = opened_article.getAttribute("text");
+
+        Assert.assertEquals(
+                "Titles do not match",
+                title_in_lists,
+                title_opened);
     }
 
     private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds)
@@ -99,5 +242,51 @@ public class FirstTest {
         String article = element.getAttribute("text");
         Assert.assertTrue("The article " + article + " does not contain word " + word,article.contains(word));
     }
+
+    protected void swipeUp(int timeOfSwipe)
+    {
+        TouchAction action = new TouchAction(driver);
+        Dimension size = driver.manage().window().getSize();
+        int x = size.width / 2;
+        int start_y = (int) (size.height * 0.8);
+        int end_y = (int) (size.height * 0.2);
+
+        action
+                .press(x, start_y)
+                .waitAction(timeOfSwipe)
+                .moveTo(x, end_y)
+                .release()
+                .perform();
+    }
+
+    protected void swipeUpQuick()
+    {
+        swipeUp(200);
+    }
+
+    protected void swipeElementToLeft(By by, String error_message)
+    {
+        WebElement element = waitForElementPresent(
+                by,
+                error_message,
+                15
+        );
+
+        int left_x = element.getLocation().getX();
+        int right_x = left_x + element.getSize().getWidth();
+        int upper_y = element.getLocation().getY();
+        int lower_y = upper_y + element.getSize().getHeight();
+        int middle_y = (upper_y + lower_y) / 2;
+
+        TouchAction action = new TouchAction(driver);
+        action
+                .press(right_x, middle_y)
+                .waitAction(400)
+                .moveTo(left_x, middle_y)
+                .release()
+                .perform();
+
+    }
+
 
 }
